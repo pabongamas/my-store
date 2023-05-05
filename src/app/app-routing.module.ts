3586e580b418616b131e113b9b11bfb8d2fcfbfb
pabongamas <pabongamas@gmail.com>
2023-05-05 17:40:08 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes,PreloadAllModules } from '@angular/router'
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import {CustomPreloadService} from './services/custom-preload.service';
 
 const routes: Routes = [
   // {path:'',redirectTo:'/home',pathMatch:'full'},
   {
     path:'',
-    loadChildren:()=>import('./website/website.module').then(m=>m.WebsiteModule)
+    loadChildren:()=>import('./website/website.module').then(m=>m.WebsiteModule),
+    data:{
+      preload:true,
+    }
   },
   {
     path:'cms',
@@ -23,7 +27,9 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy:CustomPreloadService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
